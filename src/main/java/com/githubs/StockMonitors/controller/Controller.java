d7a5994c88b15db521519733533875fe7c3f868e
Abhishek Rajbhar <rajbharabhishek40@gmail.com>
2026-03-14 21:53:29 +0530

    service class create

--- src/main/java/com/githubs/StockMonitors/controller/Controller.java
+++ src/main/java/com/githubs/StockMonitors/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.githubs.StockMonitors.controller;
 
+import com.githubs.StockMonitors.service.Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+
+    private Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHellow(){
-        return ResponseEntity.ok("Hello World");
+
+        return ResponseEntity.ok(service.getStock());
     }
 }
